b95f3a30e2813e926a62569372c0175ae022b943
ngtheanhdev <ngtheanh.dev@gmail.com>
2020-11-10 20:18:59 +0700

    Resolve Java - Data Structures 07 + 30 Days of Code - Day7-8-11

--- 1. Practice/Java/4. Data Structures/07. Java Map/Solution.java
+++ 1. Practice/Java/4. Data Structures/07. Java Map/Solution.java
@@ -2,24 +2,20 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 class Solution {
-
     public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        in.nextLine();
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            String name = in.next();
+            String name = in.nextLine();
             int phone = in.nextInt();
             map.put(name, phone);
+            in.nextLine();
         }
         while (in.hasNext()) {
-            String s = in.next();
-            if (map.containsKey(s)) {
-                System.out.println(s + "=" + map.get(s));
-            } else {
-                System.out.println("Not found");
-            }
+            String s = in.nextLine();
+            System.out.println(map.containsKey(s) ? s + "=" + map.get(s) : "Not found");
         }
-        in.close();
     }
 }
--- 1. Practice/Tutorials/30 Days of Code/Day 11 - 2D Arrays/Solution.java
+++ 1. Practice/Tutorials/30 Days of Code/Day 11 - 2D Arrays/Solution.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int largest = ((-9) * 7);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1]
+                        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if (sum >= largest) {
+                    largest = sum;
+                }
+            }
+        }
+        System.out.println(largest);
+
+        scanner.close();
+    }
 }
