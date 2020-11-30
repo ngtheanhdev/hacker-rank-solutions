2c2f56d5e423c4decb62903111741106b550a476
ngtheanh <anh.nguyen2@ntq-solution.com.vn>
2020-11-30 16:34:41 +0700

    Resolve Tutorials - 30 Days of Code - Day 16-17-18

--- 1. Practice/Tutorials/30 Days of Code/Day 16 - Exceptions - String to Integer/Solution.java
+++ 1. Practice/Tutorials/30 Days of Code/Day 16 - Exceptions - String to Integer/Solution.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        try {
+            System.out.println(Integer.parseInt(s));
+        } catch (NumberFormatException exception) {
+            System.out.println("Bad String");
+        }
+    }
 }
--- 1. Practice/Tutorials/30 Days of Code/Day 17 - More Exceptions/Solution.java
+++ 1. Practice/Tutorials/30 Days of Code/Day 17 - More Exceptions/Solution.java
@@ -1,2 +1,31 @@
-public class Solution {
+import java.util.Scanner;
+
+class Calculator {
+
+    int power(int n, int p) {
+        if (n < 0 || p < 0) {
+            throw new ArithmeticException("n and p should be non-negative");
+        }
+        return (int) Math.pow(n, p);
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
 }
