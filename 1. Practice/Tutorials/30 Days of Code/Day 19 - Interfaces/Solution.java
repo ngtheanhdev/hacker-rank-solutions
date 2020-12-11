09d386be2a0e55eca3e853e4b6ea50aec647544c
Thế Anh <giap.nguyen@fujitechjsc.com>
2020-12-11 18:01:12 +0700

    Resolve Tutorials - 30 Days of Code - Day 19-20

--- 1. Practice/Tutorials/30 Days of Code/Day 19 - Interfaces/Solution.java
+++ 1. Practice/Tutorials/30 Days of Code/Day 19 - Interfaces/Solution.java
@@ -1,2 +1,41 @@
-public class Solution {
+import java.util.Scanner;
+
+interface AdvancedArithmetic {
+    int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+        int sum = 0;
+        int sqrt = (int) Math.sqrt(n);
+
+        // Small optimization: if n is odd, we can't have even numbers as divisors
+        int stepSize = (n % 2 == 1) ? 2 : 1;
+
+        for (int i = 1; i <= sqrt; i += stepSize) {
+            if (n % i == 0) { // if "i" is a divisor
+                sum += i + n / i; // add both divisors
+            }
+        }
+
+        // If sqrt is a divisor, we should only count it once
+        if (sqrt * sqrt == n) {
+            sum -= sqrt;
+        }
+        return sum;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName());
+        System.out.println(sum);
+    }
 }
