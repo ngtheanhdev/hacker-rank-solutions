5929499d7181ec12658c496ccad49287c78e9c76
Thế Nguyễn <ngtheanh.dev@gmail.com>
2021-01-15 00:44:14 +0700

    Add statement

---
+++ Algorithms/02. Implementation/01. Grading Students/Solution.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        return null;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }).map(String::trim).map(Integer::parseInt).collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(result.stream().map(Object::toString).collect(joining("\n")) + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
